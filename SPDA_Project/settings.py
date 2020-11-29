897e45daf5a943010c136557bffacaa1d0cd276e
ali <akhmetbek.ali@gmail.com>
2020-11-29 14:27:13 +0300

    Heroku deployment

--- SPDA_Project/settings.py
+++ SPDA_Project/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'SPDA_Project.urls'
@@ -72,6 +73,8 @@ TEMPLATES = [
         },
     },
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 WSGI_APPLICATION = 'SPDA_Project.wsgi.application'
 
